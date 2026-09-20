fc42a7263744c76a9556b7e9bb61221daed22fbf
Saumya1517 <saumya.agarwal1517@gmail.com>
2026-09-20 22:41:25 +0530

    Changes added

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ pip install -r requirements.txt
 
 echo "=== [RENDER BUILD] Installing Playwright Chromium Browser ==="
 playwright install chromium
-playwright install-deps chromium || true
+
 
 echo "=== [RENDER BUILD] Collecting Static Files ==="
 python manage.py collectstatic --no-input
